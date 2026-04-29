9c58dc325cf52c6042a0939cd23f4478addd841d
sunpotflower4460-cpu <sunpotflower4460@gmail.com>
2026-04-29 20:37:13 +0900

    Load A4 print layout styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
 import "./styles/editor-quality.css";
+import "./styles/a4-print.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
