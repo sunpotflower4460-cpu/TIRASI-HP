a34fde91ad5561426ae8c46b2bf934ec2065eee1
sunpotflower4460-cpu <sunpotflower4460@gmail.com>
2026-04-29 20:14:38 +0900

    Show flyer performer profiles in wrapper

--- src/components/Flyer/FlyerWithProfiles.tsx
+++ src/components/Flyer/FlyerWithProfiles.tsx
@@ -1,6 +1,14 @@
 import type { EventData } from "../../types/event";
+import { FlyerPerformerProfiles } from "./FlyerPerformerProfiles";
 import { FlyerView } from "./FlyerView";
 
 export function FlyerWithProfiles(props: { eventData: EventData; onEdit: () => void; onPrint: () => void }) {
-  return <FlyerView eventData={props.eventData} variant="a4" onEdit={props.onEdit} onPrint={props.onPrint} />;
+  const showProfiles = props.eventData.flyerOptions.showPerformerProfiles && props.eventData.performers.length > 0;
+
+  return (
+    <>
+      <FlyerView eventData={props.eventData} variant="a4" onEdit={props.onEdit} onPrint={props.onPrint} />
+      {showProfiles ? <FlyerPerformerProfiles performers={props.eventData.performers} /> : null}
+    </>
+  );
 }
