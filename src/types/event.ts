381cdfb0acbc377c553972726635effd76667b9d
sunpotflower4460-cpu <sunpotflower4460@gmail.com>
2026-04-29 16:13:41 +0900

    Add event data types

---
+++ src/types/event.ts
@@ -0,0 +1,47 @@
+export type ScheduleItem =
+  | {
+      type: "act";
+      time: string;
+      name: string;
+    }
+  | {
+      type: "break";
+      label: string;
+    }
+  | {
+      type: "end";
+      label: string;
+    };
+
+export type SocialLink = {
+  label: string;
+  url: string;
+};
+
+export type EventData = {
+  venueSubtitle: string;
+  title: string;
+  subtitle: string;
+  volume: string;
+  date: string;
+  day: string;
+  organizer: string;
+  price: string;
+  startTime: string;
+  statusNote: string;
+  schedule: ScheduleItem[];
+  openMic: {
+    title: string;
+    time: string;
+    lastOrder: string;
+    headline: string;
+    note: string;
+    closeNote: string;
+  };
+  venue: {
+    name: string;
+    address: string;
+    url: string;
+  };
+  socials: SocialLink[];
+};
