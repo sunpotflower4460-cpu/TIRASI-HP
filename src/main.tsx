1964e83028a3649958ad664d3e0c93897de643cf
sunpotflower4460-cpu <sunpotflower4460@gmail.com>
2026-04-30 19:59:25 +0900

    Load UI UX polish stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./styles/global.css";
 import "./styles/editor-quality.css";
 import "./styles/a4-print.css";
 import "./styles/editor-mobile-fix.css";
+import "./styles/ui-ux-polish.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
